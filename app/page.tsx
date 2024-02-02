2f83aa33c431fe63d5f78fd2141ae324b72faec0
fatihcaliss <fthcls@gmail.com>
2024-02-02 12:06:01 +0300

    add HeaderMegaMenu component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Welcome } from '../components/Welcome/Welcome';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import { HeaderMegaMenu } from '@/components/HeaderMegaMenu/HeaderMegaMenu';
 
 export default function HomePage() {
   return (
     <>
+      <HeaderMegaMenu />
       <Welcome />
       <ColorSchemeToggle />
     </>
